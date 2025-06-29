3ccafbcb5a1bd76f290a1ecc7c9579755e9fc21e
Shrvan Hatte <shrvanhatte11@gmail.com>
2025-06-29 17:40:47 +0530

    Update firebase.ts

--- project/src/firebase.ts
+++ project/src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKeSRlN1ynqVAcHFvLQulIbXvZ1379M8c",
-  authDomain: "testgenius-auth.firebaseapp.com",
-  projectId: "testgenius-auth",
-  storageBucket: "testgenius-auth.firebasestorage.app",
-  messagingSenderId: "1003422488924",
-  appId: "1:1003422488924:web:d802bf83a6399a850d0999",
-  measurementId: "G-E4SWWH8CFM"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
 
 const app = initializeApp(firebaseConfig);
