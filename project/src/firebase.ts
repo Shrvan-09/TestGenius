dcaeec7a0ac49441b453a67c7496b0ae32ca3880
Shrvan Hatte <shrvanhatte11@gmail.com>
2025-07-31 15:00:02 +0530

    Update firebase.ts

--- project/src/firebase.ts
+++ project/src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKeSRlN1ynqVAcHFvLQulIbXvZ1379M8c",
-  authDomain: "testgenius-auth.firebaseapp.com",
-  projectId: "testgenius-auth",
-  storageBucket: "testgenius-auth.firebasestorage.app",
-  messagingSenderId: "1003422488924",
-  appId: "1:1003422488924:web:d802bf83a6399a850d0999",
-  measurementId: "G-E4SWWH8CFM"
+  apiKey: "YOUR API KEY",
+  authDomain: "YOUR DOMAIN",
+  projectId: "YOUR ID",
+  storageBucket: "YOUR BUCKET",
+  messagingSenderId: "YOUR ID",
+  appId: "YOUR ID",
+  measurementId: "YOUR MEAUREMENT ID"
 };
 
 
